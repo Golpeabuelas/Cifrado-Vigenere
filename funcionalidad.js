000dfc8fc63bd8117850185d74e137f1e384cd2f
Julian <chavez.garcia.julian2@gmail.com>
2024-10-18 12:18:27 -0600

    Añadiendo funcionalidades

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,64 +1,100 @@
-function Cifrar(mensaje){
+function SoloLetras(input){
+    input.value = input.value.replace(/[^a-zA-Z]/g, "");
+}
+
+async function Cifrar(mensaje){
     const Alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
     const Mensaje = mensaje;
-    const Clave = IgualarLongitudes(Mensaje, document.getElementById('ClaveCifrar').value).toUpperCase();
-    
+    let Clave = IgualarLongitudes(Mensaje, document.getElementById('ClaveCifrar').value).toUpperCase();
+
     let Cifrado = "";
-    let Temporal = "";
     let Indice = 0;
     let indiceMensaje = 0;
     let indiceClave = 0;
 
     for(let i = 0; i < Mensaje.length; i++){
-        for(let j = 0; j < Alfabeto.length; j++){
-            if(Mensaje.charAt(i) == Alfabeto.charAt(j)){
-                indiceMensaje = j;
-                break;
-            }    
+        if(/[A-ZÑ]/.test(Mensaje.charAt(i))){  
+            for(let j = 0; j < Alfabeto.length; j++){
+                if(Mensaje.charAt(i) == Alfabeto.charAt(j)){
+                    indiceMensaje = j;
+                    break;
+                }    
+            }
+
+            for(let k = 0; k < Alfabeto.length; k++){
+                if(Clave.charAt(i) == Alfabeto.charAt(k)){
+                    indiceClave = k;
+                    break;
+                }    
+            }
+
+            Indice = (indiceMensaje + indiceClave) % Alfabeto.length;
+            Cifrado = Cifrado + Alfabeto.charAt(Indice);
+        } else{
+            Cifrado = Cifrado + Mensaje.charAt(i);
         }
 
-        for(let k = 0; k < Alfabeto.length; k++){
-            if(Clave.charAt(i) == Alfabeto.charAt(k)){
-                indiceClave = k;
-                break;
-            }    
-        }
+        document.getElementById('MostrarCifrado').value =  document.getElementById('MostrarCifrado').value + Cifrado.charAt(i);
 
-        Indice = (indiceMensaje + indiceClave) % Alfabeto.length;
-        Cifrado = Cifrado + Alfabeto.charAt(Indice);
+        await new Promise(resolve => setTimeout(resolve, 300));
     }
 
     return Cifrado;
 }
 
-function Descifrar(mensaje){
-    
+async function Descifrar(mensaje) {
     const Alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
     const Mensaje = mensaje;
     const Clave = IgualarLongitudes(Mensaje, document.getElementById('ClaveDescifrar').value).toUpperCase();
-    
+
     let Cifrado = "";
     let Indice = 0;
     let indiceMensaje = 0;
     let indiceClave = 0;
 
-    for(let i = 0; i < Mensaje.length; i++){
-        for(let j = 0; j < Alfabeto.length; j++){
-            if(Mensaje.charAt(i) == Alfabeto.charAt(j)){
-                indiceMensaje = j;
-                break;
-            }    
+    const Celdas = document.getElementsByClassName("Celda");
+
+    for (let i = 0; i < Mensaje.length; i++) {
+        if (/[A-ZÑ]/.test(Mensaje.charAt(i))) {
+            for (let j = 0; j < Alfabeto.length; j++) {
+                if (Mensaje.charAt(i) == Alfabeto.charAt(j)) {
+                    indiceMensaje = j;
+                    break;
+                }
+            }
+
+            for (let k = 0; k < Alfabeto.length; k++) {
+                if (Clave.charAt(i) == Alfabeto.charAt(k)) {
+                    indiceClave = k;
+                    break;
+                }
+            }
+
+            Indice = (indiceMensaje - indiceClave + Alfabeto.length) % Alfabeto.length;
+            Cifrado += Alfabeto.charAt(Indice);
+        } else {
+            Cifrado += Mensaje.charAt(i);
         }
 
-        for(let k = 0; k < Alfabeto.length; k++){
-            if(Clave.charAt(i) == Alfabeto.charAt(k)){
-                indiceClave = k;
-                break;
-            }    
-        }
+        document.getElementById('MostrarCifrado').value += Cifrado.charAt(i);
 
-        Indice = (indiceMensaje - indiceClave + Alfabeto.length) % Alfabeto.length;
-        Cifrado = Cifrado + Alfabeto.charAt(Indice);
+        // Recorrer todas las celdas para colorearlas
+        for (let l = 0; l < Celdas.length; l++) {
+            // Colorear la celda actual
+            if (Celdas[l].value == Cifrado.charAt(i)) {
+                Celdas[l].style.backgroundColor = '#4B2E1F';
+                Celdas[l].style.color = '#FFF8E7';
+
+                // Esperar 300 ms antes de continuar
+                await new Promise(resolve => setTimeout(resolve, 300));
+
+                // Volver al estilo original
+                Celdas[l].style.backgroundColor = '';
+                Celdas[l].style.color = '';
+
+                break; // Salir del bucle cuando encontramos la celda correcta
+            }
+        }
     }
 
     return Cifrado;
@@ -94,16 +130,19 @@ async function CifrarMensaje(event) {
     let table = document.querySelector('#Matriz tbody');
     table.innerHTML = '';
 
+    document.getElementById('MostrarCifrado').value = "";
+
     const Mensaje = document.getElementById('Mensaje').value.toString().toUpperCase();
     const Palabras = Mensaje.split(" ");
     let mensaje = "";
 
     for (let i = 0; i < Palabras.length; i++) {
-        if(Palabras[i] != ""){ 
+        if(Palabras[i] != ""){
             table.innerHTML = '';
             
-            mensaje = mensaje + Cifrar(Palabras[i]) + " ";
-            MostrarMatriz(Palabras[i], IgualarLongitudes(Palabras[i], document.getElementById('ClaveCifrar').value).toUpperCase());
+            let temporal = await Cifrar(Palabras[i]);
+            mensaje = mensaje +  temporal + " ";
+            MostrarMatriz(Palabras[i], IgualarLongitudes(Palabras[i], document.getElementById('ClaveCifrar').value).toUpperCase().trim());
 
             await new Promise(resolve => setTimeout(resolve, 3000)); 
         }
@@ -118,6 +157,8 @@ async function DescifrarMensaje(event){
     let table = document.querySelector('#Matriz tbody');
     table.innerHTML = '';
 
+    document.getElementById('MostrarCifrado').value = "";
+
     const Mensaje = document.getElementById('MensajeCifrado').value.toString().toUpperCase();
     const Palabras = Mensaje.split(" ");
     let mensaje = "";
@@ -126,7 +167,8 @@ async function DescifrarMensaje(event){
         if(Palabras[i] != ""){ 
             table.innerHTML = '';
 
-            mensaje = mensaje + Descifrar(Palabras[i]) + " ";
+            let temporal = await Descifrar(Palabras[i]);
+            mensaje = mensaje +  temporal + " ";
             MostrarMatrizCifrada(Palabras[i], IgualarLongitudes(Palabras[i], document.getElementById('ClaveDescifrar').value).toUpperCase())
             
             await new Promise(resolve => setTimeout(resolve, 3000)); 
@@ -147,7 +189,6 @@ function MostrarMatriz(mensaje, clave){
         Matriz[i] = new Array(Columnas)
     }
     
-//Llenar primeras celdas 
     for(let i = 1; i < Columnas; i++){
         Matriz[0][i] = Mensaje.charAt(i - 1);
     }
@@ -156,8 +197,6 @@ function MostrarMatriz(mensaje, clave){
         Matriz[i][0] = Clave.charAt(i - 1);
     }
 
-
-//Llenar centro de la tabla
     for(let i = 1; i < Filas; i++){
         for(let j = 1; j < Columnas; j++){
             Matriz[i][j] = CifrandoMatriz(Mensaje.charAt(j - 1), Clave.charAt(i - 1));
@@ -165,6 +204,7 @@ function MostrarMatriz(mensaje, clave){
     }
 
     CrearTabla(Filas, Columnas, Matriz);
+
 }
 
 function CifrandoMatriz(CaracterM, CaracterC){
